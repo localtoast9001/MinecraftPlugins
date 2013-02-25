93e6165a85b0b02dc39d3aec58a64398f389de4a
jrowlett <jrowlett@cutlass.contrivance.org>
2013-02-24 17:55:31 -0800

    build on multiple systems.

--- src/Bitcoin/src/main/java/de/shittyco/Bitcoin/ServerErrorException.java
+++ src/Bitcoin/src/main/java/de/shittyco/Bitcoin/ServerErrorException.java
@@ -28,12 +28,6 @@ public class ServerErrorException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ServerErrorException(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
-
 	public int getCode() {
 		return this.code;
 	}
