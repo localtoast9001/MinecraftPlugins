996990f9bbce883f6f595b4698ea230806d98974
Jon Rowlett <jonrowlett@hotmail.com>
2017-11-26 16:16:59 -0800

    Included status.

---
+++ src/MinecraftServer.Status.Web/Controllers/StatusController.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StatusController.cs" company="Jon Rowlett">
+//  Copyright (C) 2017 Jon Rowlett. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MinecraftServer.Status.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Results;
+    using System.Configuration;
+    using Minecraft.Management;
+
+    /// <summary>
+    /// API to get server status.
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
+    [RoutePrefix("api/v1/status")]
+    public class StatusController : ApiController
+    {
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns>The status for the local server on the default port.</returns>
+        [Route]
+        public Task<ServerStatus> Get()
+        {
+            return ServerPingUtility.GetStatusAsync("localhost");
+        }
+    }
+}
